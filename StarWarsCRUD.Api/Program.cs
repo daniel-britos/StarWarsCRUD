b0462144a82c4f3e3288a9d82a4a665f83f818c0
Daniel Britos <danielbritospersonal@gmail.com>
2025-10-14 22:18:09 -0300

    limpiar archivos temporales y actualizar gitignore

--- StarWarsCRUD.Api/Program.cs
+++ StarWarsCRUD.Api/Program.cs
@@ -1,10 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using StarWarsCRUD.Infrastructure.Data;
 
+var builder = WebApplication.CreateBuilder(args);
 
 // AQUÍ CONECTAMOS LAS CAPAS DEL PROYECTO
 // Le decimos: "Cuando alguien pida un IPersonajeRepository, entrégale una instancia de PersonajeRepository"
 //builder.Services.AddScoped<IPersonajeRepository, PersonajeRepository>();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
